55d746fbf8b25c610ca96a25c4f8e4ca07c53954
Lawrencebyte958 <lawrence.g.f.dev@gmail.com>
2025-12-11 09:08:51 -0500

    polish as addition

--- server/server.js
+++ server/server.js
@@ -1,12 +1,32 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // server set up 
 const app = express();
-const PORT = 3000;
+const PORT = 5500;
 
-app.listen(PORT,()=>{
-    console.log(`Server listening on port: ${PORT}`)
-})
+// serve static files (CSS/HTML in this folder)
+app.use(express.static(__dirname));
+
+// serve the navbar as root (change to index.html if you have one)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'NavigationBar.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'signup.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
 export default app;
