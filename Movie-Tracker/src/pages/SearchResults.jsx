9a10e49cffb9736317ff73c423bac14de9da24e5
Mara <mara07476@gmail.com>
2025-12-10 20:18:53 -0500

    First Push

---
+++ Movie-Tracker/src/pages/SearchResults.jsx
@@ -0,0 +1,13 @@
+import MoviePoster from "../components/MoviePoster";
+import MovieTitle from "../components/MovieTitle";
+import MovieData from "../components/MovieData";
+
+export default function SearchResults() {
+  return (
+    <>
+      <MoviePoster />
+      <MovieTitle />
+      <MovieData />
+    </>
+  );
+}
