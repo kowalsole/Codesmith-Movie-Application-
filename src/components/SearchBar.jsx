b25049a2ae6e0516864bb865ca357fcef9961ad2
Annie Tadvick <annie.tadvick@gmail.com>
2025-12-15 16:23:48 -0500

    Add search bar component to modularize

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+const SearchBar = ({ query, isLoading, onQueryChange, onSubmit, onKeyPress }) => {
+  return (
+    <div className="movie-search-box">
+      <div className="movie-input-group">
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          value={query}
+          onChange={onQueryChange}
+          onKeyDown={onKeyPress}
+          className="movie-search-input"
+        />
+        <button
+          onClick={onSubmit}
+          disabled={isLoading}
+          className="movie-search-button"
+        >
+          {isLoading ? 'Searching...' : 'Search'}
+        </button>
+      </div>
+    </div>
+  );
+}
+export default SearchBar;
